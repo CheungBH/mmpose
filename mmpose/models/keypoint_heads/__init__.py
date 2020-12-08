3adc0b2e0883aa9b6b0cffeca34703a8865967ff
oliverxa <89458376@qq.com>
2020-12-08 12:14:20 +0800

    res18 with multiple duc

--- mmpose/models/keypoint_heads/__init__.py
+++ mmpose/models/keypoint_heads/__init__.py
@@ -3,8 +3,9 @@ from .bottom_up_simple_head import BottomUpSimpleHead
 from .top_down_multi_stage_head import TopDownMSMUHead, TopDownMultiStageHead
 from .top_down_simple_head import TopDownSimpleHead
 from .top_down_DUC import TopDownDUC
+from .top_down_1DUC import TopDown1DUC
 
 __all__ = [
     'TopDownSimpleHead', 'TopDownMultiStageHead', 'TopDownMSMUHead',
-    'BottomUpHigherResolutionHead', 'BottomUpSimpleHead', "TopDownDUC"
+    'BottomUpHigherResolutionHead', 'BottomUpSimpleHead', "TopDownDUC", "TopDown1DUC",
 ]

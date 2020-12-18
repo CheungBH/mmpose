44c5da182a4d0bb5e16fac376a58adff71b594e3
oliverxa <89458376@qq.com>
2020-12-18 20:47:36 +0800

    vis nam

--- auto_visualize.py
+++ auto_visualize.py
@@ -11,7 +11,7 @@ dest_path = img_path + "_vis"
 cmds = []
 
 for model, config in model_config.items():
-    model_name = model.replace("work_dirs/", "").replace("/", "-")
+    model_name = model.replace("work_dirs/", "").replace("/", "-")[:-4]
     for img_name in os.listdir(img_path):
         cmd = "python demo/top_down_img_demo_with_mmdet.py demo/mmdetection_cfg/faster_rcnn_r50_fpn_1x_coco.py \
     http://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth \
